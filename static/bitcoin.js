6f1fb519a142bbabd33bbce0e7aa34586d05c6c6
zhai <zhaimac@gmail.com>
2018-03-21 05:49:02 -0500

    Render stock price

--- static/bitcoin.js
+++ static/bitcoin.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             console.log(i+ " " +bpi[i])
             $("#BitResults").append(i+ " " +bpi[i]+ "<br>")
         };
-
+        renderChart(bpi);
     };
 
     function bitconButtonClicked(event) {
@@ -22,22 +22,25 @@ $(document).ready(function () {
     $("#BitCoinButton").click(bitconButtonClicked);
 
 
+    function renderChart(bpi) {
+    console.log(bpi.keys)
+
     Highcharts.chart('container', {
         chart: {
             type: 'line'
         },
         title: {
-            text: 'Monthly Average Temperature'
+            text: '30 days Bitcoin Average Price'
         },
         subtitle: {
             text: 'Source: butaire.com'
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            categories: Object.keys(bpi)
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: 'Price in USD'
             }
         },
         plotOptions: {
@@ -45,16 +48,17 @@ $(document).ready(function () {
                 dataLabels: {
                     enabled: true
                 },
-                enableMouseTracking: false
+                enableMouseTracking: true
             }
         },
         series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            name: 'Btcoin',
+            data: Object.values(bpi)
         }, {
-            name: 'London',
+            name: 'Baseine',
             data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     });
+    }
 
 })
